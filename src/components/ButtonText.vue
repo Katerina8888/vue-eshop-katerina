6af7433b147bad0c493116a66a3076e7b815e7aa
katerina <katerina@igloonet.cz>
2025-02-03 16:19:02 +0100

    refactor: button styles and new component - shared button styles to main.css - new button component for symbols

--- src/components/ButtonText.vue
+++ src/components/ButtonText.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="absolute bottom-1 not-only:md:bottom-3 left-1/2 transform -translate-x-1/2 z-10 bg-white px-4 py-1 hover:bg-purple-600 hover:text-white transition-all duration-500 cursor-pointer rounded-xl text-black font-bold uppercase"
+    class="absolute bottom-1 not-only:md:bottom-3 left-1/2 transform -translate-x-1/2 px-4 py-1"
   >
     Buy
   </button>
