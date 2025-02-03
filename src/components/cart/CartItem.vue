e4c4dcb35819392a7acaf642a007dbc390308ccb
katerina <katerina@igloonet.cz>
2025-02-03 16:19:40 +0100

    feat: cart items component

---
+++ src/components/cart/CartItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <li class="list-none py-3 grid grid-cols-4 gap-1">
+    <ButtonSymbolsValue>x</ButtonSymbolsValue>
+    <div>Produkt</div>
+    <div>2x</div>
+    <div>100 €</div>
+  </li>
+</template>
+
+<script setup lang="ts">
+import ButtonSymbolsValue from '../store/ButtonSymbolsValue.vue'
+</script>
---
+++ src/components/cart/CartItemsWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul class="w-2/3">
+    <CartItem></CartItem>
+    <CartItem></CartItem>
+    <CartItem></CartItem>
+    <CartItem></CartItem>
+    <CartItem></CartItem>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import CartItem from './CartItem.vue'
+</script>
