2da210e12c7f36b93edac92c5835c44aa6208af8
katerina <katerina@igloonet.cz>
2025-02-03 16:20:30 +0100

    feat: use new components in cartview

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="flex">
+    <CartItemsWrapper></CartItemsWrapper>
+
+    <TotalPrice></TotalPrice>
   </main>
 </template>
+
+<script setup lang="ts">
+import CartItemsWrapper from '@/components/cart/CartItemsWrapper.vue'
+import TotalPrice from '@/components/cart/TotalPrice.vue'
+</script>
