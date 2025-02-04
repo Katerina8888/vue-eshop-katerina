ebee29c359ccf528454c4d11064026d8dcf41ee8
katerina <katerina@igloonet.cz>
2025-02-04 15:09:05 +0100

    refactor: convert cart store to composition API with reactive state management

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,35 +1,44 @@
 import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
 
-export const useCartStore = defineStore('cart', {
-  state: () => ({
-    quantities: {} as Record<number, number>,
-  }),
-  getters: {
-    getQuantity: (state) => {
-      return (productId: number) => state.quantities[productId] || 0
-    },
-    totalQuantity: (state) => {
-      return Object.values(state.quantities).reduce((total, quantity) => total + quantity, 0)
-    },
-  },
-  actions: {
-    setQuantity(productId: number, quantity: number) {
-      if (quantity > 0) {
-        this.quantities[productId] = quantity
-      } else {
-        delete this.quantities[productId]
-      }
-    },
-    increaseQuantity(productId: number) {
-      if (!this.quantities[productId]) {
-        this.quantities[productId] = 0
-      }
-      this.quantities[productId]++
-    },
-    decreaseQuantity(productId: number) {
-      if (this.quantities[productId] > 0) {
-        this.quantities[productId]--
-      }
-    },
-  },
+export const useCartStore = defineStore('cart', () => {
+  const quantities = ref<Record<number, number>>({})
+
+  const getQuantity = (productId: number) => {
+    return quantities.value[productId] || 0
+  }
+
+  const totalQuantity = computed(() => {
+    return Object.values(quantities.value).reduce((total, quantity) => total + quantity, 0)
+  })
+
+  const setQuantity = (productId: number, quantity: number) => {
+    if (quantity > 0) {
+      quantities.value[productId] = quantity
+    } else {
+      delete quantities.value[productId]
+    }
+  }
+
+  const increaseQuantity = (productId: number) => {
+    if (!quantities.value[productId]) {
+      quantities.value[productId] = 0
+    }
+    quantities.value[productId]++
+  }
+
+  const decreaseQuantity = (productId: number) => {
+    if (quantities.value[productId] > 0) {
+      quantities.value[productId]--
+    }
+  }
+
+  return {
+    quantities,
+    getQuantity,
+    totalQuantity,
+    setQuantity,
+    increaseQuantity,
+    decreaseQuantity,
+  }
 })
