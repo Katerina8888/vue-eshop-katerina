e2b29781d4541966029f98dfe9988e41f1dc6772
katerina <katerina@igloonet.cz>
2025-02-04 11:30:48 +0100

    style: remove unnecessary tag

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,24 +1,22 @@
 <template>
-  <header>
-    <div class="flex justify-between p-4 text-2xl">
-      <span class="font-bold text-purple-500">E-SHOP</span>
-      <nav>
-        <RouterLink
-          to="/store"
-          :class="{ underline: $route.path === '/store' }"
-          class="px-2 md:px-4 py-2"
-        >
-          Store
-        </RouterLink>
-        <RouterLink
-          to="/cart"
-          :class="{ underline: $route.path === '/cart' }"
-          class="px-2 md:px-4 py-2"
-        >
-          Cart
-        </RouterLink>
-      </nav>
-    </div>
+  <header class="flex justify-between p-4 text-2xl mb-12">
+    <span class="font-bold text-purple-500">E-SHOP</span>
+    <nav>
+      <RouterLink
+        to="/store"
+        :class="{ underline: $route.path === '/store' }"
+        class="px-2 md:px-4 py-2"
+      >
+        Store
+      </RouterLink>
+      <RouterLink
+        to="/cart"
+        :class="{ underline: $route.path === '/cart' }"
+        class="px-2 md:px-4 py-2"
+      >
+        Cart
+      </RouterLink>
+    </nav>
   </header>
 </template>
 
