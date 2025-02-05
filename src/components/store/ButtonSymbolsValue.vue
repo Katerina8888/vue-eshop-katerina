f1eff846a83b636d26b9e25cf389d48c0331e261
katerina <katerina@igloonet.cz>
2025-02-05 09:45:15 +0100

    feat: update ButtonSymbolsValue styling for improved visibility and interaction

--- src/components/store/ButtonSymbolsValue.vue
+++ src/components/store/ButtonSymbolsValue.vue
@@ -1,3 +1,5 @@
 <template>
-  <button class="p-1 m-auto w-8"><slot></slot></button>
+  <button class="!bg-purple-500 !hover:bg-purple-200 !text-white p-1 m-auto w-8">
+    <slot></slot>
+  </button>
 </template>
