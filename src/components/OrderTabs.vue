7cbb35a6b5f8435e7d6c5aa3dd7b29cf717bbbc2
katerina <katerina@igloonet.cz>
2025-02-07 08:59:58 +0100

    feat: add BigButton component and integrate it into OrderTabs for navigation

---
+++ src/components/OrderTabs.vue
@@ -0,0 +1,9 @@
+<template>
+  <ul class="flex justify-center space-x-4">
+    <BigButton>Order číslo1</BigButton>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import BigButton from './BigButton.vue'
+</script>
